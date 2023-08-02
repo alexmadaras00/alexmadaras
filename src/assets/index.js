b1e8e6cbbccc9e995e258372b5166d06391b1e63
Alex Madaras <62357167+MadarasAlex@users.noreply.github.com>
2023-08-02 22:28:16 +0300

    feature-alpha-version

---
+++ src/assets/index.js
@@ -0,0 +1,156 @@
+import bwmap from './backgrounds/bw-map.jpeg';
+import nairobi from './backgrounds/nairobi.png';
+import whiteabstract from './backgrounds/white-abstract.png';
+import worldmap from './backgrounds/world-map.png';
+import back from './backgrounds/back.png';
+import mobileVideo from './backgrounds/background_mobiles.mp4';
+
+
+import logo from './logo/logo.png';
+import logotext from './logo/logo-text-black.png';
+
+import shaq from './personal/shaq.png';
+import alex from './personal/alex.png';
+
+import backend from './icons/backend.png';
+import ux from './icons/ux.png';
+import frontend from './icons/frontend.png';
+import prototyping from './icons/prototyping.png';
+import github from './icons/github.png';
+import close from './icons/close.png';
+import download from './icons/download.png';
+import downloadHover from './icons/downloadHover.png';
+import pineapple from './icons/pineapple.png';
+import pineappleHover from './icons/pineappleHover.png';
+import menu from './icons/menu.png';
+import resume from './icons/resume.png';
+import send from './icons/send.png';
+import sendHover from './icons/sendHover.png';
+import android from './icons/android.png';
+import analysis from './icons/analysis.png';
+import preparation from './icons/preparation.png';
+import visualization from './icons/visualization.png';
+import management from './icons/management.png';
+import gameDev from './icons/game.png';
+
+import css from './tech/css.png';
+import docker from './tech/docker.png';
+import figma from './tech/figma.png';
+import git from './tech/git.png';
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import nodejs from './tech/nodejs.png';
+import reactjs from './tech/reactjs.png';
+import redux from './tech/redux.png';
+import tailwind from './tech/tailwind.png';
+import typescript from './tech/typescript.png';
+import graphql from './tech/graphql.png';
+import postgresql from './tech/postgresql.png';
+import rubyrails from './tech/rubyrails.png';
+import kotlin from './tech/kotlin.png';
+import firebase from './tech/firebase.png';
+import adobexd from './tech/adobexd.png';
+import sql from './tech/sql.png';
+import java from './tech/java.png';
+import android_studio from './tech/android_studio.png';
+import colab from './tech/colab.png';
+import jira from './tech/jira.png';
+import python from './tech/python.png';
+
+import coverhunt from './company/coverhunt.png';
+import dcc from './company/dcc.png';
+import kelhel from './company/kelhel.png';
+import microverse from './company/microverse.png';
+import uvt from './company/uvt.png';
+import erasmus from './company/erasmus.png';
+import twente from './company/twente.png';
+import victory_square from './company/victorysquare.png';
+
+
+import leaderboard from './projects/leaderboard.png';
+import math from './projects/math-magicians.png';
+import movie from './projects/movie-metro.png';
+import nyeusi from './projects/nyeusi.png';
+import space from './projects/space-hub.png';
+import eia from './projects/eia.png';
+import azul2 from './projects/azul2.png';
+import vrades from './projects/vrades-mockups.png';
+import datascience from './projects/datascience.png';
+import table from './projects/tables.png';
+import star from './projects/star_schema.png';
+
+export {
+    analysis,
+    azul2,
+    android,
+    bwmap,
+    uvt,
+    erasmus,
+    datascience,
+    star,
+    table,
+    victory_square,
+    twente,
+    nairobi,
+    whiteabstract,
+    vrades,
+
+    worldmap,
+    mobileVideo,
+    logo,
+    logotext,
+    alex,
+    shaq,
+    backend,
+    back,
+    ux,
+    frontend,
+    prototyping,
+    github,
+    close,
+    gameDev,
+    download,
+    downloadHover,
+    pineapple,
+    pineappleHover,
+    eia,
+    menu,
+    management,
+    visualization,
+    preparation,
+    resume,
+    send,
+    sendHover,
+    css,
+    docker,
+    figma,
+    git,
+    graphql,
+    html,
+    javascript,
+    nodejs,
+    kotlin,
+    adobexd,
+    sql,
+    jira,
+    firebase,
+    android_studio,
+    python,
+    colab,
+    java,
+    postgresql,
+    rubyrails,
+    reactjs,
+    redux,
+    tailwind,
+    typescript,
+    leaderboard,
+    math,
+    movie,
+    nyeusi,
+    space,
+    coverhunt,
+    dcc,
+    kelhel,
+    microverse,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,288 @@
+import {
+    ux,
+    prototyping,
+    nodejs,
+    kotlin,
+    adobexd,
+    sql,
+    jira,
+    firebase,
+    android_studio,
+    python,
+    colab,
+    java,
+    git,
+    figma,
+    postgresql,
+    graphql,
+    leaderboard,
+    math,
+    movie,
+    nyeusi,
+    android,
+    analysis,
+    preparation,
+    visualization,
+    management,
+    gameDev,
+    erasmus,
+    uvt,
+    twente,
+    victory_square, azul2, vrades, datascience,
+} from '../assets';
+
+export const navLinks = [
+    {
+        id: 'about',
+        title: 'About',
+    },
+    {
+        id: 'projects',
+        title: 'Projects',
+    },
+    {
+        id: 'contact',
+        title: 'Contact',
+    },
+];
+
+const services = [
+    {
+        title: 'Android Developer',
+        icon: android
+    },
+    {title: 'Game Developer',
+        icon: gameDev
+    },
+    {
+        title:'UI/UX Design',
+        icon: ux,
+    },
+    {
+        title: 'Software Prototyping',
+        icon: prototyping,
+    },
+    {
+        title: 'Data Analysis',
+        icon: analysis
+    },
+    {
+        title: 'Data Preparation',
+        icon: preparation
+    },
+    {
+        title: 'Data Visualization',
+        icon: visualization
+    },
+    {
+        title:'Software Management',
+        icon: management
+    }
+];
+
+const technologies = [
+    {
+        name: 'Kotlin',
+        icon: kotlin,
+    },
+    {
+      name:'Android Studio',
+      icon: android_studio,
+    },
+    {
+        name: 'Firebase',
+        icon: firebase,
+    },
+    {
+        name: 'MySQL',
+        icon: sql,
+    },
+    {
+        name: 'Java',
+        icon: java,
+    },
+    {
+        name:'Jira',
+        icon: jira,
+    },
+    {
+        name: 'GIT',
+        icon: git,
+    },
+    {
+        name: 'Adobe XD',
+        icon: adobexd,
+    },
+    {
+        name: 'Node JS',
+        icon: nodejs,
+    },
+    {
+        name: 'Python',
+        icon: python,
+    },
+    {
+        name: 'Google Colab',
+        icon: colab,
+    },
+    {
+        name: 'graphql',
+        icon: graphql,
+    },
+    {
+        name: 'PostgreSQL',
+        icon: postgresql,
+    },
+    {
+        name: 'Figma',
+        icon: figma,
+    },
+];
+
+const experiences = [
+    {
+        title: 'Master Software Technology',
+        company_name: 'University of Twente',
+        icon: twente    ,
+        iconBg: '#333333',
+        date: 'Sep 2022 - Present',
+    },
+    {
+        title: 'Mobile Developer Intern',
+        company_name: 'Victory Square Partners',
+        icon: victory_square,
+        iconBg: '#333333',
+        date: 'Aug 2021 - Sep 2021',
+    },
+    {
+        title: 'Erasmus Exchange in Lisbon',
+        company_name: 'IADE - Universidade Europeia ',
+        icon: erasmus,
+        iconBg: '#333333',
+        date: 'Sep 2020 - Feb 2021',
+    },
+    {
+        title: 'Bachelor Computer Science',
+        company_name: 'West University of Timisoara',
+        icon: uvt,
+        iconBg: '#333333',
+        date: 'Sep 2019 - Aug 2022',
+    }
+];
+
+const projects = [
+    {
+        id: 'project-1',
+        name: 'AzulApp',
+        description: '2D Board Game developed as part of a team of 5 students. ',
+        tags: [
+            {
+                name: 'java',
+                color: 'blue-text-gradient',
+            },
+            {
+                name: 'jira',
+                color: 'green-text-gradient',
+            },
+            {
+                name: 'git',
+                color: 'pink-text-gradient',
+            },
+        ],
+        image: azul2,
+        repo: 'https://github.com/alexmadaras00/AzulApp',
+        // demo: 'https://shaqdeff.github.io/KomiKult/',
+    },
+    {
+        id: 'project-2',
+        name: 'VRADES',
+        description:
+            'Android app.',
+        tags: [
+            {
+                name: 'react',
+                color: 'blue-text-gradient',
+            },
+            {
+                name: 'restapi',
+                color: 'green-text-gradient',
+            },
+            {
+                name: 'scss',
+                color: 'pink-text-gradient',
+            },
+        ],
+        image: vrades,
+        repo: 'https://github.com/shaqdeff/Leaderboard',
+         // demo: 'https://alexmadaras00.github.io/business-intelligence',
+    },
+    {
+        id: 'project-3',
+        name: 'Business Intelligence',
+        description: 'Data analysis and visualization project on Classic Models Inc.\'s business performance using the BSC approach, with strategic recommendations for management. Data stored on PostgreSQL.',
+        tags: [
+            {
+                name: 'nextjs',
+                color: 'blue-text-gradient',
+            },
+            {
+                name: 'supabase',
+                color: 'green-text-gradient',
+            },
+            {
+                name: 'css',
+                color: 'pink-text-gradient',
+            },
+        ],
+        image: datascience,
+        repo: 'https://github.com/alexmadaras00/Business-Intelligence',
+        demo: 'https://alexmadaras00.github.io/business-intelligence',
+    },
+    {
+        id: 'project-4',
+        name: 'IQQuizApp',
+        description: `Android App with different types of IQ Tests. Developed in a team of 3 as part of an University Project.`,
+        tags: [
+            {
+                name: 'nextjs',
+                color: 'blue-text-gradient',
+            },
+            {
+                name: 'supabase',
+                color: 'green-text-gradient',
+            },
+            {
+                name: 'css',
+                color: 'pink-text-gradient',
+            },
+        ],
+        image: movie,
+        repo: 'https://github.com/alexmadaras00/IQQuizApp',
+        // demo: 'https://movie-metro.netlify.app/',
+    },
+    {
+        id: 'project-5',
+        name: 'SpyForHire',
+        description:
+            'SpyForHire: The companion app for the Windows game EIA. Developed using Kotlin, SQL, and NodeJS as part of a multiplatform project during my Erasmus Mobility.',
+        tags: [
+            {
+                name: 'nextjs',
+                color: 'blue-text-gradient',
+            },
+            {
+                name: 'supabase',
+                color: 'green-text-gradient',
+            },
+            {
+                name: 'css',
+                color: 'pink-text-gradient',
+            },
+        ],
+        image: nyeusi,
+        repo: 'https://github.com/alexmadaras00/SpyForHire-CompanionApp',
+        // demo: 'https://shaqdeff.github.io/Nyeusi-Fest-Site/',
+    },
+];
+
+export {services, technologies, experiences, projects};
