47023d01f01c53cfe9f50b4122863d38d52e1d44
Alex Madaras <62357167+MadarasAlex@users.noreply.github.com>
2025-05-28 12:52:11 +0200

    Completed the About Screen
    - Added a flex layout for the cards
    - Shaped the content

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,11 @@ export default {
         primary: '#050816',
         secondary: '#aaa6c3',
         tertiary: '#151030',
+        customGreen:{
+          10: '#69896E',
+          100: '#dde7d9',
+          200: '#c2d1bc',
+        },
         'black-100': '#100d25',
         'black-200': '#090325',
         'white-100': '#f3f3f3',
